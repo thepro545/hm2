7ff7588995c8e5925b89a9f43f623007e43456de
Max <thepro545@mail.ru>
2021-12-23 11:02:06 +0300

    les 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -7,7 +7,7 @@ public class Main {
         byte test1 = 100;
         short test2 = 32000;
         int test3 = 2000000;
-        long test4 = 900000000L;
+        long test4 = 9000000000L;
         float test5 = 2312.2021f;
         double test6 = 23122021.23122021;
         boolean test7 = true;
