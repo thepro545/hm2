450c137236722b6e3be7f3798d3bde5c2021ed20
Max <thepro545@mail.ru>
2021-12-23 11:17:24 +0300

    les 3

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,15 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        byte bananas = 80;
+        byte milk = 105;
+        byte icecream = 100;
+        byte eggs = 70;
+
+        float breakfast = bananas * 5 + milk * 2 + icecream * 2 + eggs * 4;
+
+        System.out.println("Вес завтрака = " + breakfast + " грамм");
+        System.out.println("Вес завтрака = " + breakfast/1000 + " кг");
     }
 }
