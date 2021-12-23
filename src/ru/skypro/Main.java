abda25df844aa90dd464f4c848205fa713202801
Max <thepro545@mail.ru>
2021-12-23 11:23:40 +0300

    les 4

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,16 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int weight = 7000;
+        int lost1 = 250;
+        int lost2 = 500;
+
+        int res1 = 7000/250;
+        int res2 = 7000/500;
+
+        System.out.println("Нужно " + res1 + " дней, чтобы похудеть (при 250 гр в день)");
+        System.out.println("Нужно " + res2 + " дней, чтобы похудеть (при 500 гр в день)");
+        System.out.println("В среднем нужен " + ((res1+res2)/2) + " день, чтобы похудеть");
     }
 }
