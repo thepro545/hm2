5079272db66f240218367e2ae3347fa023e15d03
Max <thepro545@mail.ru>
2021-12-23 11:07:44 +0300

    les 2

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,11 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        float boxerWeight1 = 78.2f;
+        float boxerWeight2 = 82.7f;
+
+        System.out.println("Общий вес бойцов = " + (boxerWeight1 + boxerWeight2) + " кг");
+        System.out.println("Разница в весе бойцов = " + (boxerWeight2 - boxerWeight1) + " кг");
     }
 }
